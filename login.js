80481994906418c4357a655b7d9db260d6e09772
Administrator <Administrator@WIN-K7GD1U2J94O>
2023-05-16 02:11:10 +0800

    Co-authored-by: ChrisMDFK <ChrisMDFK@users.noreply.github.com>

--- login.js
+++ login.js
@@ -34,7 +34,7 @@ async function verifyUser(event) {
 
     if (session_login.Code == -3) {
         
-        return showConfirm(session_login);
+        return showConfirm(session_login , password);
     }
     else if (session_login.Code < 0) {
         alert(session_login.Result);
@@ -55,15 +55,19 @@ async function verifyUser(event) {
 
     return false; // 防止表單提交
 }
-async function showConfirm(session_login) {
-    if (confirm("帳號已登入,是否強制登出?")) {
-
+async function showConfirm(session_login ,pwd) {
+    
+    if (confirm("帳號已登入,是否強制登出?")) {  
+        console.log(session_login);     
         let userdata = {
-            "ID": session_login.Data[0].ID
+            "ID": session_login.Data[0].ID,
+            "Password": pwd
         };
+        console.log(userdata);
         data = await postDataToAPI(session_logout_post_url, userdata);
         console.log(data);
-
+        session_login = await postDataToAPI(session_login_post_url, userdata);
+        console.log(session_login);
         sessionStorage.setItem('loggedID', session_login.Data[0].ID);
         sessionStorage.setItem('loggedName', session_login.Data[0].Name);
         sessionStorage.setItem('loggedEmployer', session_login.Data[0].Employer);
